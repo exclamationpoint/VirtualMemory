5a3a24b17abf979e93843e00ed9f8844498a2c4d
Chuck Gustner <cegustner@gmail.com>
2020-04-23 18:43:53 -0500

    Moved variables from TLB into formatter

--- src/TLB.java
+++ src/TLB.java
@@ -6,14 +6,29 @@ import java.util.Hashtable;
  */
 public class TLB {
 	Hashtable<Integer, Integer> memory;
+	int pageFault;
+	int pageHit;
+	int[] pageNumber;
+	int[] frameNumber;
+	int[] entryAge;
+	static int FRAME_SIZE = 256;
+	static int TOTAL_FRAME_COUNT = 256;
+	static String PAGE_MASK = "0xFF00";
+	static String OFFSET_MASK = " 0xFF";
+	static int SHIFT = 8;
+	static int TLB_SIZE = 16;
+	static int PAGE_TABLE_SIZE = 256;
+	static int PAGE_READ_SIZE = 256;
 
 	public TLB() {
 		memory = new Hashtable<>();
+		pageFault = 0;
+		pageHit = 0;
 	}
 
 	public void populateTable(int[] keys, int[] value) {
 		for (int i = 0; i < keys.length; i++) {
-			memory.put(keys[0], value[0]);
+			memory.put(keys[i], value[i]);
 		}
 	}
 
