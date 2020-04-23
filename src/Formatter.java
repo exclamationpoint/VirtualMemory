fdd2f4f236b518b90bfd5d9527cb3c3900adbd1a
Chuck Gustner <cegustner@gmail.com>
2020-04-23 18:26:17 -0500

    Added a few things

---
+++ src/Formatter.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+
+/**
+ * @author Owner
+ *
+ */
+public class Formatter {
+	String data;
+
+	public Formatter(String data) {
+		this.data = data;
+	}
+
+	public String toMemAddress() {
+		int halfway = data.length() / 2;
+		String output = data.substring(halfway, data.length() - 1);
+		return output;
+	}
+
+	public String[] pageOffsetArray() {
+		String address = toMemAddress();
+		String[] output = new String[2];
+		output[0] = address.substring(0, (address.length() / 2) - 1);
+		output[1] = address.substring(address.length() / 2, address.length() - 1);
+		return output;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
- * 
+ *
  */
 
 /**
@@ -15,8 +18,21 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		File file = new File("addresses.txt");
+		File bin = new File("BACKING_STORE.bin");
 		FileInput data = new FileInput(file);
-		TLB memory = new TLB();
+		BufferedInputStream bfi;
+		try {
+			FileInputStream binInput = new FileInputStream(bin);
+			int BUFFER_SIZE = 32 * 1024; // 32kb
+			bfi = new BufferedInputStream(binInput, BUFFER_SIZE);
+			System.out.println(bfi.read());
+			System.out.println(data.getNext());
+			System.out.println();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
